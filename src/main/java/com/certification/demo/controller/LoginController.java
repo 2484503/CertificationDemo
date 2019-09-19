3ab83731c4f875c5edcb45bb6ce0e8c752471839
lijiannan <2484503@qq.com>
2019-09-19 10:40:04 +0800

    Add PasswordHash.java

--- src/main/java/com/certification/demo/controller/LoginController.java
+++ src/main/java/com/certification/demo/controller/LoginController.java
@@ -3,7 +3,7 @@ package com.certification.demo.controller;
 import com.certification.demo.model.entity.User;
 import com.certification.demo.sdk.LoginClient;
 import com.certification.demo.service.UserService;
-import com.certification.demo.utils.MD5Util;
+import com.certification.demo.utils.PasswordHash;
 import com.certification.demo.utils.TokenProccessorUtil;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,14 +23,24 @@ public class LoginController implements LoginClient {
 
     @Override
     public String login(String username, String password) {
-        String passwordMd5 = MD5Util.getMD5(password);
 
-        User user = userService.getUserByNamePassword(username,passwordMd5);
+        User user = userService.getUserByNamePassword(username, null);
 
-        if (user == null){
+        if (user == null) {
             return null;
         }
+
+        try {
+            // 校验用户密码是否正确
+            if (!PasswordHash.validatePassword(password, user.getPassword())) {
+                return null;
+            }
+        } catch (Exception e) {
+            return null;
+        }
+
         String token = TokenProccessorUtil.getInstance().makeToken();
+
         return token;
     }
 }
